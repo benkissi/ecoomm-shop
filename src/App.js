ee9b660a743d609cc81261a8c21558e18202b892
benkissi <benkissi111@gmail.com>
2019-08-06 18:42:26 -0400

    finish signin handle submit

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ class App extends Component {
               id: snapshot.id,
               ...snapshot.data()  
             }
-          }, ()=> console.log(this.state))
+          })
         })
         
       }
