b485ec3acc8e10a5b2a94aabc446c74ba6fb786b
benkissi <benkissi111@gmail.com>
2019-08-07 07:49:22 -0400

    push

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import { Route, Switch } from 'react-router-dom'
 
+
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component' 
@@ -50,7 +51,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser}/>
+        <Header />
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
