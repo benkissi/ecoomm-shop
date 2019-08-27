f4e9a28695860ae22a7dc08bfdcb279bbe69af66
benkissi <benkissi111@gmail.com>
2019-08-27 01:10:20 +0000

    separate client and server

---
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import StripeCheckout from 'react-stripe-checkout'
+import axios from 'axios'
+
+const StripeCheckoutButton = ({ price }) => {
+    const priceForStripe = price * 100
+    const publishableKey = 'pk_test_vOMA7ibFbWdXg6s4A3x7F2fY00FC6mJ9yR'
+
+    const onToken = token => {
+        axios({
+            url: 'payment',
+            method: 'post',
+            data: {
+                amount: priceForStripe,
+                token
+            }
+        }).then(response => {
+            alert('Payment Successful')
+        }).catch(error => {
+            console.log('Payment error ', JSON.parse(error))
+            alert('There was am issue with your payment')
+        })
+    }
+
+    return (
+        <StripeCheckout 
+            label = 'Pay Now'
+            name = 'Top Shark Clothing Ltd'
+            billingAddress
+            shippingAddress
+            image = 'https://svgshare.com/i/CUz.svg'
+            description = {`Your total is $${price}`}
+            amount = {priceForStripe}
+            panelLabel = 'Pay Now'
+            token = {onToken}
+            stripeKey = {publishableKey}
+        />
+    )
+}
+
+export default StripeCheckoutButton
